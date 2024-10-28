b7afdb98bf3a805bb8c6a508993600446b8fc053
Will Worrell <willrequaworrell@gmail.com>
2024-10-27 22:43:29 -0400

    taking out Date required as optional tasks don't need a date

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   title: { type: String, required: true },
-  dueDate: { type: Date, required: true },
+  dueDate: { type: Date },
   column: { type: String, enum: ['today', 'upcoming', 'optional'], required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
