6efe194723541cbf9712cb8d6eb13a14144d707a
Will Worrell <willrequaworrell@gmail.com>
2024-11-19 23:55:53 -0500

    revert cors options change

--- server.js
+++ server.js
@@ -8,27 +8,10 @@ const baseRoutes = require('./routes/baseRoutes')
 //test
 const app = express();
 
-const allowedOrigins = [
-    'https://klarr.vercel.app',
-    'http://localhost:3000' // for local development
-];
 
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    optionsSuccessStatus: 200
-};
 
-app.use(cors(corsOptions));
-// app.options('*', cors());
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use('/api/tasks', taskRoutes)
 app.use('/', baseRoutes)
