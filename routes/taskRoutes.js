423d8b4fa15b023058960e2c4deec706f7f0c129
Will Worrell <willrequaworrell@gmail.com>
2024-11-20 12:53:56 -0500

    logging +

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -42,7 +42,7 @@ router.post('/', async (req, res) => {
 
 // Get all tasks for a user
 router.get('/:userId', async (req, res) => {
-  console.log("getting all tasks for:", req)
+  console.log("getting all tasks for:", req.params.userId)
 	try {
 		const tasks = await Task.find({ userId: req.params.userId });
 		const sortedTasks = tasks.sort((a, b) => {
